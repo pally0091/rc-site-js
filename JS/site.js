c95669412e4260ebe2704980164a398cb2320fe0
Debashis Roy <goodquote39@hotmail.com>
2022-10-07 19:04:13 +0600

    page done

---
+++ JS/site.js
@@ -0,0 +1,30 @@
+const list = ('/db.json');
+
+const items = 
+    fetch(list)
+        .then(res => res.json())
+        .then(data => loadProducts(data))
+
+const loadProducts = products => {
+    const productContainer = document.getElementById("products")
+            
+    productContainer.innerHTML='';
+    products.forEach(product => {
+        console.log(product)
+     
+        const productBox =document.createElement('div');
+        
+        productBox.classList.add('box')
+        productBox.innerHTML = `
+            <div>
+                <img src="${product.pimage}"
+                <div>
+                    <h3>${product.name}</h3>
+                    <p>${product.details}</p>
+                    <p>Price : ${product.price}</p>
+                </div>
+            </div>
+        `
+        productContainer.appendChild(productBox)
+    });
+}
